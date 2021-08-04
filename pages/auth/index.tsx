ba0e4998bb183acce88a573309f2760e17fb8d15
illia.zaliatskyi <illia.zaliatskyi@codeit.pro>
2021-08-04 19:00:28 +0300

    adding styles, bootstrap and materialize

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,5 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Col, Container, Row } from 'react-bootstrap'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 const Auth = () => {
-  return <main className={'auth'}>This is auth page!</main>
+  return (
+    <Container className={'auth'}>
+      <Row>
+        <Col>
+          <FontAwesomeIcon icon={faCoffee} />
+          this is auth page
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Auth
