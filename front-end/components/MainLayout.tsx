3fd5229d7a916979564080aad4d142782bdf62d6
illia.zaliatskyi <illia.zaliatskyi@codeit.pro>
2021-08-04 15:45:46 +0300

    husky settings

--- front-end/components/MainLayout.tsx
+++ front-end/components/MainLayout.tsx
@@ -1,15 +1,16 @@
+import { Fragment } from 'react'
 import Head from 'next/head'
 import Navigation from './navigation/Navigation'
-const blabla = ''
+
 const MainLayout = ({ children, title = '' }) => {
   return (
-    <>
+    <Fragment>
       <Head>
         <title>Hotel Network App | {title}</title>
       </Head>
       <Navigation />
       <main>{children}</main>
-    </>
+    </Fragment>
   )
 }
 
