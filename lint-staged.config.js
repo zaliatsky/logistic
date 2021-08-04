328bff036fd9de211009f22e5a7dc351430588f5
illia.zaliatskyi <illia.zaliatskyi@codeit.pro>
2021-08-04 16:32:22 +0300

    configs for prettier, lint-staged and husky precommit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Type check TypeScript files
-  'front-end/**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
@@ -9,6 +9,6 @@ module.exports = {
   ],
 
   // Format MarkDown and JSON
-  'front-end/**/*.(md|json)': (filenames) =>
+  '**/*.(md|json)': (filenames) =>
     `yarn prettier --write ${filenames.join(' ')}`,
 }
