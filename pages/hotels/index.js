7017dba9e20345a7bdee036df977b46ad27fe4dc
illia.zaliatskyi <illia.zaliatskyi@codeit.pro>
2021-08-19 17:19:17 +0300

    removed unneed parts of code

--- pages/hotels/index.js
+++ pages/hotels/index.js
@@ -1,7 +1,7 @@
 import MainLayout from '../../front-end/components/MainLayout'
 
 const Hotels = () => {
-  return <MainLayout title={'Hotels'}>this is all hotels page!</MainLayout>
+  return <MainLayout title="Hotels">this is all hotels page!</MainLayout>
 }
 
 export default Hotels
--- pages/map/index.js
+++ pages/map/index.js
@@ -1,7 +1,7 @@
 import MainLayout from '../../front-end/components/MainLayout'
 
 const Map = () => {
-  return <MainLayout title={'Map'}>This is map page!</MainLayout>
+  return <MainLayout title="Map">This is map page!</MainLayout>
 }
 
 export default Map
